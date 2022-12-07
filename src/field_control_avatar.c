d63e654a26af52e8ebb2c025f3e82f316ee61b9a
David Volovskiy <devolov@gmail.com>
2022-12-06 21:57:05 -0500

    Running is now a toggle

--- src/field_control_avatar.c
+++ src/field_control_avatar.c
@@ -129,6 +129,10 @@ void FieldGetPlayerInput(struct FieldInput *input, u16 newKeys, u16 heldKeys)
         input->dpadDirection = DIR_WEST;
     else if (heldKeys & DPAD_RIGHT)
         input->dpadDirection = DIR_EAST;
+    if (newKeys & B_BUTTON)
+        FlagSet(FLAG_PREV_RUN_BTN_STATE);
+    // DV 20221202 Using this location to set the flag that the B button was pressed.
+    // (Likely can be all done in field_player_avater.c, but this is faster)
 }
 
 int ProcessPlayerFieldInput(struct FieldInput *input)
@@ -149,7 +153,6 @@ int ProcessPlayerFieldInput(struct FieldInput *input)
 
     if (TryRunOnFrameMapScript() == TRUE)
         return TRUE;
-
     if (input->pressedBButton && TrySetupDiveEmergeScript() == TRUE)
         return TRUE;
     if (input->tookStep)
