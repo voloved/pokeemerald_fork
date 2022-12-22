fc28566bce51a5909fe3f3c4609aa3ee5da3ec9b
David Volovskiy <devolov@gmail.com>
2022-12-22 00:58:31 -0500

    Changed catch percent back to 2.5

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -9910,7 +9910,7 @@ static void Cmd_handleballthrow(void)
                 break;
             case ITEM_THIEF_BALL:  // If used on trainer, it's 2.5x; if used on a wild Pokemon, it's 1x
                 if (gBattleTypeFlags & BATTLE_TYPE_TRAINER)
-                    ballMultiplier = 100;
+                    ballMultiplier = 25;
                 else
                     ballMultiplier = 10;
                 break;
