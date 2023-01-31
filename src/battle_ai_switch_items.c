be07f487be6d82aca10bff4c060d93f26386cf6e
David Volovskiy <devolov@gmail.com>
2023-01-31 08:40:20 -0500

    Switch AI is now only the same on easy difficulty

--- src/battle_ai_switch_items.c
+++ src/battle_ai_switch_items.c
@@ -644,7 +644,7 @@ u8 GetMostSuitableMonToSwitchInto(void)
     u8 invalidMons;
     u16 move;
     bool8 checkedAllMonForSEMoves = FALSE;  // We have checked all Pokemon in the party for if they have a super effective move
-    bool8 hardDiff = VarGet(VAR_DIFFICULTY) == DIFFICULTY_HARD;
+    bool8 easyDifficulty = VarGet(VAR_DIFFICULTY) == DIFFICULTY_EASY;
 
     if (*(gBattleStruct->monToSwitchIntoId + gActiveBattler) != PARTY_SIZE)
         return *(gBattleStruct->monToSwitchIntoId + gActiveBattler);
@@ -692,10 +692,10 @@ u8 GetMostSuitableMonToSwitchInto(void)
 
     while (invalidMons != 0x3F) // All mons are invalid.
     {
-        if (hardDiff)
-            bestDmg = 255;
-        else
+        if (easyDifficulty)
             bestDmg = TYPE_MUL_NO_EFFECT;
+        else
+            bestDmg = 255;
         bestMonId = PARTY_SIZE;
         // Find the mon whose type is the most suitable offensively.
         for (i = firstId; i < lastId; i++)
@@ -715,11 +715,12 @@ u8 GetMostSuitableMonToSwitchInto(void)
                 ModulateByTypeEffectiveness(gBattleMons[opposingBattler].type1, type1, type2, &typeDmg);
                 ModulateByTypeEffectiveness(gBattleMons[opposingBattler].type2, type1, type2, &typeDmg);
 
-                /* Possible bug: this comparison gives the type that takes the most damage, when
+                /* Possible bug with Vanilla: this comparison gives the type that takes the most damage, when
                 a "good" AI would want to select the type that takes the least damage. Unknown if this
-                is a legitimate mistake or if it's an intentional, if weird, design choice */
-                if ((bestDmg < typeDmg && !hardDiff)
-                ||  (bestDmg > typeDmg &&  hardDiff))
+                is a legitimate mistake or if it's an intentional, if weird, design choice.
+                Now: That possible bug only takes place if playing on easy difficulty. It's fixed on all others*/
+                if ((bestDmg > typeDmg && !easyDifficulty)
+                ||  (bestDmg < typeDmg &&  easyDifficulty))
                 {
                     bestDmg = typeDmg;
                     bestMonId = i;
@@ -745,7 +746,7 @@ u8 GetMostSuitableMonToSwitchInto(void)
                 return bestMonId; // Has both the typing and at least one super effective move or has a typing that's super effective and no other Pokemon have a super effective move
 
             invalidMons |= gBitTable[bestMonId]; // Sorry buddy, we want something better.
-            if (invalidMons == 0x3F && !checkedAllMonForSEMoves && hardDiff)  // If we already checked all for a super effective move, then use the one with the best typing
+            if (invalidMons == 0x3F && !checkedAllMonForSEMoves && !easyDifficulty)  // If we already checked all for a super effective move, then use the one with the best typing
             {
                 invalidMons = 0;
                 checkedAllMonForSEMoves = TRUE;
