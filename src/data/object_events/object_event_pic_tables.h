58e37df33f0c5f4847cded3ae0423f030aa0cfe3
David Volovskiy <devolov@gmail.com>
2023-04-05 08:27:48 -0400

    Made Missingno side follower sprite the same as front

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -5467,8 +5467,8 @@ static const struct SpriteFrameImage sPicTable_Missingno[] = {
     overworld_frame(gObjectEventPic_Missingno, 4, 4, 1),
     overworld_frame(gObjectEventPic_Missingno, 4, 4, 2),
     overworld_frame(gObjectEventPic_Missingno, 4, 4, 3),
-    overworld_frame(gObjectEventPic_Missingno, 4, 4, 4),
-    overworld_frame(gObjectEventPic_Missingno, 4, 4, 5),
+    overworld_frame(gObjectEventPic_Missingno, 4, 4, 0),
+    overworld_frame(gObjectEventPic_Missingno, 4, 4, 1),
 };
 
 static const struct SpriteFrameImage sPicTable_DeoxysOld[] = {
