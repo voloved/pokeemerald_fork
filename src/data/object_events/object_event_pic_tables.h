075e5482647c3fbf12a76bfbcd3fab59c028ca35
David Volovskiy <devolov@gmail.com>
2023-04-05 08:23:19 -0400

    Animated Missingno follower

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -5464,11 +5464,11 @@ static const struct SpriteFrameImage sPicTable_Sylveon[] = {
 };
 static const struct SpriteFrameImage sPicTable_Missingno[] = {
     overworld_frame(gObjectEventPic_Missingno, 4, 4, 0),
-    overworld_frame(gObjectEventPic_Missingno, 4, 4, 0),
-    overworld_frame(gObjectEventPic_Missingno, 4, 4, 0),
-    overworld_frame(gObjectEventPic_Missingno, 4, 4, 0),
-    overworld_frame(gObjectEventPic_Missingno, 4, 4, 0),
-    overworld_frame(gObjectEventPic_Missingno, 4, 4, 0),
+    overworld_frame(gObjectEventPic_Missingno, 4, 4, 1),
+    overworld_frame(gObjectEventPic_Missingno, 4, 4, 2),
+    overworld_frame(gObjectEventPic_Missingno, 4, 4, 3),
+    overworld_frame(gObjectEventPic_Missingno, 4, 4, 4),
+    overworld_frame(gObjectEventPic_Missingno, 4, 4, 5),
 };
 
 static const struct SpriteFrameImage sPicTable_DeoxysOld[] = {
