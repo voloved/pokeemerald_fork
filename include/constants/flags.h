b2e5db0c46d38c1a0ecface7d86aa39b3ec59700
David Volovskiy <devolov@gmail.com>
2023-05-14 11:18:31 -0400

    Mew under truck with strength

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1181,9 +1181,9 @@
 #define FLAG_ITEM_MOSSDEEP_STEVENS_HOUSE_HM08                       0x46D // Unused Flag, leftover from R/S. HM08 is given to the player directly in Emerald
 #define FLAG_ITEM_ROUTE_119_NUGGET                                  0x46E
 #define FLAG_ITEM_ROUTE_104_POTION                                  0x46F
-#define FLAG_UNUSED_0x470                                           0x470 // Unused Flag
+#define FLAG_LILYCOVE_HARBOR_TRUCK_MOVED                            0x470
 #define FLAG_ITEM_ROUTE_103_PP_UP                                   0x471
-#define FLAG_UNUSED_0x472                                           0x472 // Unused Flag
+#define FLAG_LILYCOVE_HARBOR_MEW_SEEN                               0x472
 #define FLAG_ITEM_ROUTE_108_STAR_PIECE                              0x473
 #define FLAG_ITEM_ROUTE_109_POTION                                  0x474
 #define FLAG_ITEM_ROUTE_110_ELIXIR                                  0x475
