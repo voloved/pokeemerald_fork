4429d0398000fd324d540292a0ab7c52d15d3feb
David Volovskiy <devolov@gmail.com>
2023-07-24 22:16:58 -0400

    Made the PokeVial's text change so it's not saying uses for one use

--- include/strings.h
+++ include/strings.h
@@ -960,8 +960,6 @@ extern const u8 gText_Anna[];
 
 extern const u8 gText_ExpShareTurnOn[];
 extern const u8 gText_ExpShareTurnOff[];
-extern const u8 gText_PokeVial_Success[];
-extern const u8 gText_PokeVial_Failure[];
 extern const u8 gText_CleanseTagTurnOn[];
 extern const u8 gText_CleanseTagTurnOff[];
 extern const u8 gText_PokeDollTurnOn[];
