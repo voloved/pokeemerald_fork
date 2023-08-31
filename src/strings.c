54c6adc379b9b0489eb92e3798fa807702a21adb
David Volovskiy <devolov@gmail.com>
2023-08-31 17:03:38 -0400

    Added Ability to Move Held Items

--- src/strings.c
+++ src/strings.c
@@ -1895,3 +1895,7 @@ const u8 gText_Berry[] = _("BERRY");
 const u8 gText_Berries[] = _("BERRIES");
 const u8 gText_Nickname[] = _("NICKNAME");
 const u8 gText_Relearn[] = _("RELEARN");
+const u8 gMenuText_Move[] = _("MOVE");
+const u8 gText_MoveItemWhere[] = _("Move item to where?");
+const u8 gText_XsYAnd[] = _("{STR_VAR_1}'s\n{STR_VAR_2} and\l");
+const u8 gText_XsYWereSwapped[] = _("{STR_VAR_1}'s\l{STR_VAR_2} were swapped!{PAUSE_UNTIL_PRESS}");
