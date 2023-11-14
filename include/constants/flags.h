citrusbolt/pokeheliodor

Remove rumble test to restore intro multiboot loader and add flag for GBP detection

include/constants/flags.h
@@ -74,8 +74,8 @@
 #define FLAG_USED_EREADER    0x3E
 #define FLAG_DISPLAY_DOT_CODE 0x3F
 #define FLAG_BLENDER_UPGRADED 0x40
-#define FLAG_COMPLETED_ROAMER_QUEST 0x41 // Unused Flag
-#define FLAG_UNUSED_0x042    0x42 // Unused Flag
+#define FLAG_COMPLETED_ROAMER_QUEST 0x41
+#define FLAG_USED_GAME_BOY_PLAYER 0x42
 #define FLAG_UNUSED_0x043    0x43 // Unused Flag
 #define FLAG_UNUSED_0x044    0x44 // Unused Flag
 #define FLAG_UNUSED_0x045    0x45 // Unused Flag

voloved/pokeemerald_fork

include/constants/flags.h
@@ -1251,7 +1251,7 @@
 #define FLAG_MID_BATTLE_EVOLUTION                                   0x4B2
 #define FLAG_GOT_MINT_FROM_JAIME                                    0x4B3
 #define FLAG_RUMBLE                                                 0x4B4
-#define FLAG_UNUSED_0x4B5                                           0x4B5 // Unused Flag
+#define FLAG_USED_GAME_BOY_PLAYER                                   0x4B5
 #define FLAG_UNUSED_0x4B6                                           0x4B6 // Unused Flag
 #define FLAG_UNUSED_0x4B7                                           0x4B7 // Unused Flag
 #define FLAG_UNUSED_0x4B8                                           0x4B8 // Unused Flag